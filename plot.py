8c8b78c4a81353cb144538883499fafb69fcd819
Tyan <960007nathan@gmail.com>
2019-11-29 02:23:32 +0800

    all points in the tracks array

--- plot.py
+++ plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# open file & read
+## open file & read
 
 fin = open('shoshone.gpx')
 
@@ -16,7 +16,37 @@ maxlon = float(bounds[3].strip('maxlon="/>'))
 
 #print(minlat,minlon,maxlat,maxlon)
 
-# get every points in an ndarray
+## get every points in a list, then ndarray
 
+for i in range(6):
+    fin.readline()
+
+tracks_list = []
+index = 0
+while True:
+    hrzt_line = fin.readline()
+    if hrzt_line.strip() == '</trkseg>': break
+
+    hrzt_list = hrzt_line.strip('<> trkpt\n').split()
+    #print(hrzt_list)
+    tracks_list.append( float(hrzt_list[0].strip('lat="')) )
+    tracks_list.append( float(hrzt_list[1].strip('lon="')) )
+
+    ele_line = fin.readline().strip('<el>/ \n')
+    tracks_list.append( float(ele_line) )
+
+    fin.readline()
+    
+    index += 1
+
+#for i in range(3):
+#    print(tracks_list[i])
+
+tracks = np.array(tracks_list).reshape(index,3)
+
+#for i in range(6):
+#    print(tracks[i])
+
+#print(tracks[3,1])
 
 fin.close()
