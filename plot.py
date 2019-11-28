355c8ac35c98101602c655496307381e9fd6453d
Tyan <960007nathan@gmail.com>
2019-11-28 21:55:33 +0800

    read file and get bounds

--- plot.py
+++ plot.py
@@ -1,5 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 # open file & read
 
-# get the numbers of lat,lon,ele
+fin = open('shoshone.gpx')
+
+for i in range(8):
+    fin.readline()
+
+bounds = str(fin.readline()).strip(' <bounds').split()
+minlat = float(bounds[0].strip('minlat="'))
+minlon = float(bounds[1].strip('minlo="'))
+maxlat = float(bounds[2].strip('maxlt="'))
+maxlon = float(bounds[3].strip('maxlon="/>'))
+
+print(minlat,minlon,maxlat,maxlon)
+
+# get every points in an ndarray
+
+fin.close()
 
 # create an array that every row is a point
